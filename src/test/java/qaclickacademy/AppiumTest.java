6f5f52abb447e01bb20296151ae5b73001471ac7
uday <uday.jegarakal@gmail.com>
2021-11-29 22:06:16 -0500

    pushing code for the first time

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeappAndroid()
+	{
+		System.out.println("NativeappAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+}
